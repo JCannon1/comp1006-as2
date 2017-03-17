eb725b9aec1fc7e0f36350eba9f2f78c8235d995
JCannon1 <jessecannon1@hotmail.com>
2017-03-16 21:49:04 -0400

    added login.php

---
+++ validate.php
@@ -0,0 +1,31 @@
+<?php ob_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+require_once ('db.php');
+
+$sql = "SELECT userId, password FROM users WHERE username = :username";
+
+$cmd = $conn->prepare($sql);
+$cmd->bindParam(':username', $username, PDO::PARAM_STR, 50);
+$cmd->execute();
+
+$user = $cmd->fetch();
+
+if (password_verify($password, $user['password'])) {
+
+    session_start();
+    $_SESSION['userId'] = $user['userId'];
+
+    $_SESSION['username'] = $username;
+    header('location:albums.php');
+}
+else {
+    header('location:login.php?invalid=true');
+    exit();
+}
+
+$conn = null;
+
+ob_flush(); ?>
