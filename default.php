3cd0f62d57729789930d6b138e03a33fc1217f00
JCannon1 <jessecannon1@hotmail.com>
2017-03-29 12:59:18 -0400

    added default.php upload.php and save-upload.php

---
+++ default.php
@@ -0,0 +1,15 @@
+<?php 
+$pageTitle = 'COMP1006 | ';
+require_once('header.php'); ?>
+
+<main class="container">
+
+<section class="jumbotron">
+    <h1></h1>
+    <p></p>
+</section>
+</main>
+
+<?php
+require_once('footer.php');
+?>
