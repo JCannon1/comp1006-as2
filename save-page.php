434931c35ffdb9234ca017aef33cf3da7f0ee3b0
JCannon1 <jessecannon1@hotmail.com>
2017-04-02 16:37:59 -0400

    added footer social media links

---
+++ save-page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload Details</title>
+</head>
+<body>
+
+<?php
+print_r($_FILES['anyFile']);
+
+$name = $_FILES['anyFile']['name'];
+echo "$name<br />";
+
+$tmp_name = $_FILES['anyFile']['tmp_name'];
+echo "$tmp_name<br />";
+
+$name = uniqid("", true) . "-$name";
+
+move_uploaded_file($tmp_name, "uploads/$name");
+?>
+
+</body>
+</html>
