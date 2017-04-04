a1eba969a0fdb162c60fb5480236e5b77825c17a
JCannon1 <jessecannon1@hotmail.com>
2017-04-04 14:53:18 -0400

    upload logo edit

--- logo.php
+++ logo.php
@@ -3,6 +3,18 @@
 $pageTitle = 'Upload Logo';
 require_once('header.php'); ?>
 
+<?php 
+if (file_exists("images/" . $_FILES["file"]["name"]))
+    {
+    $_FILES["file"]["name"] . " File name already exists. ";
+    }
+else {
+    move_uploaded_file($_FILES["file"]["tmp_name"],
+    "images/" . $_FILES["file"]["name"]);
+    echo "Stored in: " . "images/" . $_FILES["file"]["name"];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +23,15 @@ require_once('header.php'); ?>
 </head>
 <body>
 <h1>Upload Logo</h1>
-<form method="post" action="header.php" enctype="multipart/form-data">
-    <label for"anyFile">Choose a Logo: </label>
-    <input name="anyFile" id="anyFile" type="file" />
+<form method="post" action="control-pannel.php" enctype="multipart/form-data">
+    <label for"file">Choose a Logo: </label>
+    <input name="file" id="file" type="file" />
     <button>Upload</button>
 </form>
+
+<form id="uploadLogo" name="uploadLogo" action="">
+<input type="hidden" id="filename" value="<?echo $_FILES["file"]["name"];?>"/>
+
 </body>
 </html>
 
