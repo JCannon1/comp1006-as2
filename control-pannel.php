54dc084fbf1e119431bbd8cb4eddc463add1d89a
JCannon1 <jessecannon1@hotmail.com>
2017-04-04 15:15:46 -0400

    edited control-pannel.php

--- control-pannel.php
+++ control-pannel.php
@@ -1,4 +1,14 @@
-<a href="admin-user.php">Add, Edit and Delete Site Administrators</a>
-<a href="page-details.php">Manage Site Content</a>
-<a href="logo.php">Upload a new logo</a>
-<a href="admin-user.php">View the public site</a>
+<?php ob_start();
+
+$pageTitle = 'Admin Users List';
+require_once('header.php'); ?>
+
+<div class="list-group">
+  <a href="admin-user.php" class="list-group-item active">Add, Edit and Delete Site Administrators</a>
+  <a href="page-details.php" class="list-group-item">Manage Site Content</a>
+  <a href="logo.php" class="list-group-item">Upload a new logo</a>
+  <a href="admin-user.php" class="list-group-item">View the public site</a>
+</div>
+
+<?php require_once('footer.php');
+ob_flush(); ?>
