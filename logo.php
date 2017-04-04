58bb45c29fc9375f467f8be1c6d7cac9d8e82dcf
JCannon1 <jessecannon1@hotmail.com>
2017-04-04 19:32:04 -0400

    edited upload and save upload

--- logo.php
+++ logo.php
@@ -24,8 +24,8 @@ require_once('header.php'); ?>
 <body>
 <h1>Upload Logo</h1>
 <form method="post" action="save-upload.php" enctype="multipart/form-data">
-    <label for"file">Choose a Logo: </label>
-    <input name="file" id="file" type="file" />
+    <label for"anyFile">Choose a Logo: </label>
+    <input name="anyFile" id="anyFile" type="file" />
     <button>Upload</button>
 </form>
 
--- save-upload.php
+++ save-upload.php
@@ -7,17 +7,20 @@
 <body>
 
 <?php
-print_r($_FILES['file']);
+print_r($_FILES['anyFile']);
 
-$name = $_FILES['file']['name'];
+$name = $_FILES['anyFile']['name'];
 echo "$name<br />";
 
-$tmp_name = $_FILES['file']['tmp_name'];
+$type = $_FILES['anyFile']['type'];
+echo "$type<br />";
+
+$tmp_name = $_FILES['anyFile']['tmp_name'];
 echo "$tmp_name<br />";
 
 $name = uniqid("", true) . "-$name";
 
-move_uploaded_file($tmp_name, "images/$name");
+move_uploaded_file($tmp_name, "uploads/$name");
 ?>
 
 </body>
