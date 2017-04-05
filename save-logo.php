2a8df0e30266fe997fdb5d79fe49d68bc45df1e9
JCannon1 <jessecannon1@hotmail.com>
2017-04-05 14:33:37 -0400

    edited save-logo

--- save-logo.php
+++ save-logo.php
@@ -7,14 +7,8 @@
 <body>
 
 <?php
-
-try {
-    $logo = null; 
-
-    $ok = true;
-
-    if (!empty($_FILES['logo']['name'])) {
-        $name = $_FILES['logo']['name'];
+if (!empty($_FILES['anyFile']['name'])) {
+        $name = $_FILES['anyFile']['name'];
 
         // use end() and explode() to get the letters after the last period i.e. the file extension
         $arr = end(explode('.', $name));
@@ -33,55 +27,20 @@ try {
         }
 
         // size check
-        $size = $_FILES['logo']['size'];
+        $size = $_FILES['anyFile']['size'];
         if ($size > 2048000) {
             echo 'Logo Image must be less than 2 MB<br />';
             $ok = false;
         }
 
         // rename to unique file name
-        $logo = uniqid("") . "-$name";
+        $name = uniqid("") . "-$name";
 
         // copy to /covers folder
-        $tmp_name = $_FILES['logo']['tmp_name'];
-        move_uploaded_file($tmp_name, "logos/$name");
+        $tmp_name = $_FILES['anyFile']['tmp_name'];
+        move_uploaded_file($tmp_name, "uploads/$name");
 
     }
-
-    if ($ok == true) {
-        // connect to db - dbtype, server address, dbname, username, password
-        require_once('db.php');
-
-        // set up an SQL instruction to save the new album - INSERT or UPDATE
-        if (empty($logoId)) {
-            $sql = "INSERT INTO logos (logoId, logo) VALUES (:logo);";
-        } else {
-            $sql = "UPDATE logos SET logo = :logo WHERE logoId = :logoId";
-        }
-
-        // pass the input variables to the SQL command
-        $cmd = $conn->prepare($sql);
-        $cmd->bindParam(':logo', $logo, PDO::PARAM_STR, 50);
-
-        // populate id if we have one
-        if (!empty($logoId)) {
-            $cmd->bindParam(':logoId', $logoId, PDO::PARAM_INT);
-        }
-
-        // execute the INSERT
-        $cmd->execute();
-
-        // disconnect
-        $conn = null;
-
-        // show a message to the user
-        //echo 'Logo Saved';
-        header('location:logo.php');
-    }
-}
-catch (exception $e) {
-    header('location:error.php');
-}
 ?>
 
 </body>
