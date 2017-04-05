16820f89e98f3ae7f983e49e4b34722fb1db3013
JCannon1 <jessecannon1@hotmail.com>
2017-04-05 13:44:26 -0400

    edited save-upload.php

--- save-upload.php
+++ save-upload.php
@@ -7,52 +7,40 @@
 <body>
 
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["anyFile"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["anyFile"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+if (!empty($_FILES['logo']['name'])) {
+        $name = $_FILES['logo']['name'];
+
+        // use end() and explode() to get the letters after the last period i.e. the file extension
+        $arr = end(explode('.', $name));
+        //echo $arr;
+
+        // convert the extension to lower case
+        $type = strtolower($arr);
+        //echo $type;
+
+        // allow jpg / png / gif / svg
+        $fileTypes = ['jpg', 'png', 'gif', 'svg'];
+
+        if (!in_array($type, $fileTypes)) {
+            echo 'Invalid Image Type<br />';
+            $ok = false;
+        }
+
+        // size check
+        $size = $_FILES['logo']['size'];
+        if ($size > 2048000) {
+            echo 'Logo Image must be less than 2 MB<br />';
+            $ok = false;
+        }
+
+        // rename to unique file name
+        $logo = uniqid("") . "-$name";
+
+        // copy to /covers folder
+        $tmp_name = $_FILES['logo']['tmp_name'];
+        move_uploaded_file($tmp_name, "logos/$logo");
+
     }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["anyFile"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["anyFile"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["anyFile"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-// display image
-$image = $_FILES["anyFile"]["name"]; 
-$img = "upload/".$image;
-echo "<img src=\"uploads/$img\>";
 ?>
 
 </body>
